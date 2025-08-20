72bb9fdadad6447823b9cd570c0aa998b539f9a6
mbarillasvelasquez <mbarillasvelasquez@alumni.scu.edu>
2025-08-20 12:40:28 +0200

    Work on Gridap Functions for ECM

--- scripts/MB Ex 3/0-EM_PB_ECM.jl
+++ scripts/MB Ex 3/0-EM_PB_ECM.jl
@@ -21,6 +21,8 @@ using WriteVTK
 using JLD2
 using Plots
 using Base.Threads
+ ENV["PYTHON"] = "C:/Users/mjbarillas/AppData/Local/Programs/Python/Python312/python.exe"
+using PyCall
 
 #endregion
 
@@ -239,6 +241,21 @@ function Training_Set_Read(v)
     return D_x, D_T
 end
 
+function Assemble_contributions()
+    folder = "scripts/MB Ex 3/POD_red_Solutions/V1_r_contributions/"
+    k,l = 17, 12
+    f_list = [0.9,1.0,1.1]
+    C_list = []
+    for f in f_list
+        sol = jldopen(folder*"MaterialParameter$f/RedParam_k_$(k)_l_$(l)_f_$f.jld2")
+        C_ = reduce(vcat,reduce.(hcat,sol["r_contri_list"]))
+        push!(C_list,C_)
+    end
+    C = reduce(vcat,C_list)
+    b = vec(sum(C,dims=2))
+    return C, b
+end
+
 #endregion
 
 ##
@@ -273,6 +290,36 @@ end
 #endregion
 
 ##
+
+##
+
+#region ECM Matrices
+
+function RondomizedSVD(C,tol=1e-8)
+    pushfirst!(pyimport("sys")."path", "C:/Users/mjbarillas/Documents/GitHub/Mimosa/scripts/Scratch")
+    RandomizedSVD_module = pyimport("randomized_singular_value_decomposition")
+    u, _, _, _ = RandomizedSVD_module.RandomizedSingularValueDecomposition().Calculate(C', truncation_tolerance=tol)
+    return u
+end
+
+function ECM_Selection(u,tol=1e-6)
+    pushfirst!(pyimport("sys")."path", "C:/Users/mjbarillas/Documents/GitHub/Mimosa/scripts/Scratch")
+    ECM_module = pyimport("empirical_cubature_method")
+    ecm = ECM_module.EmpiricalCubatureMethod(ECM_tolerance=tol)
+    ecm.SetUp(
+        ResidualsBasis=u,
+        InitialCandidatesSet=nothing,
+        constrain_sum_of_weights=true,
+        constrain_conditions=false
+    )
+    ecm.Run()
+    return   ecm.z, ecm.w
+end
+
+#endregion
+
+##
+
 
 ##
 
@@ -507,6 +554,307 @@ end
 #endregion
 ##
 
+##
+
+#region ECM Gridap Functions
+
+function Gridap.FESpaces.assemble_matrix!(mat,a::SparseMatrixAssembler,matdata,z,w)
+    LinearAlgebra.fillstored!(mat,zero(eltype(mat)))
+    assemble_matrix_add!(mat,a,matdata,z,w)
+end
+
+function Gridap.FESpaces.assemble_matrix_add!(mat,a::SparseMatrixAssembler,matdata,z,w)
+    numeric_loop_matrix!(mat,a,matdata,z,w)
+    Gridap.FESpaces.create_from_nz(mat)
+end
+
+function Gridap.FESpaces.numeric_loop_matrix!(A,a::SparseMatrixAssembler,matdata,z,w)
+    strategy = Gridap.FESpaces.get_assembly_strategy(a)
+    # println("Check!")
+    h=0
+    for (cellmat,_cellidsrows,_cellidscols) in zip(matdata...)
+        cellidsrows = Gridap.FESpaces.map_cell_rows(strategy,_cellidsrows)
+        cellidscols = Gridap.FESpaces.map_cell_cols(strategy,_cellidscols)
+        @assert length(cellidscols) == length(cellidsrows)
+        @assert length(cellmat) == length(cellidsrows)
+        if length(cellmat) > 0
+            rows_cache = array_cache(cellidsrows)
+            cols_cache = array_cache(cellidscols)
+            vals_cache = array_cache(cellmat)
+            mat1 = getindex!(vals_cache,cellmat,1)
+            rows1 = getindex!(rows_cache,cellidsrows,1)
+            cols1 = getindex!(cols_cache,cellidscols,1)
+            # if h==0
+            #     println(rows1)
+            # end
+            add! = AddEntriesMap(+)
+            add_cache = return_cache(add!,A,mat1,rows1,cols1)
+            caches = add_cache, vals_cache, rows_cache, cols_cache
+            Gridap.FESpaces._numeric_loop_matrix!(A,caches,cellmat,cellidsrows,cellidscols,z,w)
+            h+=1
+        end
+        # println("Count 1 = $h")
+    end
+    A
+end
+
+@noinline function Gridap.FESpaces._numeric_loop_matrix!(mat,caches,cell_vals,cell_rows,cell_cols,z,w)
+    cells = zip(z,w)
+    add_cache, vals_cache, rows_cache, cols_cache = caches
+    add! = AddEntriesMap(+)
+    for cell in cells
+    rows = getindex!(rows_cache,cell_rows,cell[1])
+    cols = getindex!(cols_cache,cell_cols,cell[1])
+    vals = getindex!(vals_cache,cell_vals,cell[1])
+    evaluate!(add_cache,add!,mat,cell[2]*vals,rows,cols)
+    end
+end
+
+function Gridap.FESpaces.allocate_matrix(a::SparseMatrixAssembler,matdata,z,w)
+    m1 = Gridap.FESpaces.nz_counter(get_matrix_builder(a),(get_rows(a),get_cols(a)))
+    symbolic_loop_matrix!(m1,a,matdata,z,w)
+    m2 = Gridap.FESpaces.nz_allocation(m1)
+    symbolic_loop_matrix!(m2,a,matdata,z,w)
+    m3 = Gridap.FESpaces.create_from_nz(m2)
+    m3
+end
+
+function Gridap.FESpaces.symbolic_loop_matrix!(A,a::SparseMatrixAssembler,matdata,z,w)
+    get_mat(a::Tuple) = a[1]
+    get_mat(a) = a
+    if Gridap.FESpaces.LoopStyle(A) == Gridap.FESpaces.DoNotLoop()
+      return A
+    end
+    strategy = Gridap.FESpaces.get_assembly_strategy(a)
+    for (cellmat,_cellidsrows,_cellidscols) in zip(matdata...)
+      cellidsrows = Gridap.FESpaces.map_cell_rows(strategy,_cellidsrows)
+      cellidscols = Gridap.FESpaces.map_cell_cols(strategy,_cellidscols)
+      @assert length(cellidscols) == length(cellidsrows)
+      if length(cellidscols) > 0
+        rows_cache = array_cache(cellidsrows)
+        cols_cache = array_cache(cellidscols)
+        mat1 = get_mat(first(cellmat))
+        rows1 = getindex!(rows_cache,cellidsrows,1)
+        cols1 = getindex!(cols_cache,cellidscols,1)
+        touch! = TouchEntriesMap()
+        touch_cache = return_cache(touch!,A,mat1,rows1,cols1)
+        caches = touch_cache, rows_cache, cols_cache
+        Gridap.FESpaces._symbolic_loop_matrix!(A,caches,cellidsrows,cellidscols,mat1,z,w)
+      end
+    end
+    A
+end
+
+@noinline function Gridap.FESpaces._symbolic_loop_matrix!(
+  A, caches, cell_rows, cell_cols, mat1, z, w
+)
+    cells = zip(z,w)
+    touch_cache, rows_cache, cols_cache = caches
+    touch! = TouchEntriesMap()
+    for cell in cells
+    rows = getindex!(rows_cache,cell_rows,cell[1])
+    cols = getindex!(cols_cache,cell_cols,cell[1])
+    evaluate!(touch_cache,touch!,A,mat1,rows,cols)
+    end
+end
+
+function Gridap.FESpaces.assemble_vector!(b,a::SparseMatrixAssembler,vecdata,z,w)
+    fill!(b,zero(eltype(b)))
+    assemble_vector_add!(b,a,vecdata,z,w)
+end
+
+function Gridap.FESpaces.assemble_vector_add!(b,a::SparseMatrixAssembler,vecdata,z,w)
+    numeric_loop_vector!(b,a,vecdata,z,w)
+    Gridap.FESpaces.create_from_nz(b)
+end
+
+function Gridap.FESpaces.numeric_loop_vector!(b,a::SparseMatrixAssembler,vecdata,z,w)
+    strategy = Gridap.FESpaces.get_assembly_strategy(a)
+    for (cellvec, _cellids) in zip(vecdata...)
+      cellids = Gridap.FESpaces.map_cell_rows(strategy,_cellids)
+      if length(cellvec) > 0
+        rows_cache = array_cache(cellids)
+        vals_cache = array_cache(cellvec)
+        vals1 = getindex!(vals_cache,cellvec,1)
+        rows1 = getindex!(rows_cache,cellids,1)
+        add! = AddEntriesMap(+)
+        add_cache = return_cache(add!,b,vals1,rows1)
+        caches = add_cache, vals_cache, rows_cache
+        Gridap.FESpaces._numeric_loop_vector!(b,caches,cellvec,cellids,z,w)
+      end
+    end
+    b
+end
+
+@noinline function Gridap.FESpaces._numeric_loop_vector!(
+  vec, caches, cell_vals, cell_rows, z, w
+)
+    cells = zip(z,w)
+    add_cache, vals_cache, rows_cache = caches
+    @assert length(cell_vals) == length(cell_rows)
+    add! = AddEntriesMap(+)
+    for cell in cells
+    rows = getindex!(rows_cache,cell_rows,cell[1])
+    vals = getindex!(vals_cache,cell_vals,cell[1])
+    evaluate!(add_cache,add!,vec,cell[2]*vals,rows)
+    end
+end
+
+
+function Gridap.FESpaces.allocate_vector(a::SparseMatrixAssembler,vecdata,z,w)
+    v1 = Gridap.FESpaces.nz_counter(get_vector_builder(a),(get_rows(a),))
+    symbolic_loop_vector!(v1,a,vecdata,z,w)
+    v2 = Gridap.FESpaces.nz_allocation(v1)
+    symbolic_loop_vector!(v2,a,vecdata,z,w) 
+    v3 = Gridap.FESpaces.create_from_nz(v2)
+    v3
+end
+
+function Gridap.FESpaces.symbolic_loop_vector!(b,a::SparseMatrixAssembler,vecdata,z,w)
+    get_vec(a::Tuple) = a[1]
+    get_vec(a) = a
+    if Gridap.FESpaces.LoopStyle(b) == Gridap.FESpaces.DoNotLoop()
+      return b
+    end
+    strategy = Gridap.FESpaces.get_assembly_strategy(a)
+    for (cellvec,_cellids) in zip(vecdata...)
+      cellids = Gridap.FESpaces.map_cell_rows(strategy,_cellids)
+      if length(cellids) > 0
+        rows_cache = array_cache(cellids)
+        vec1 = get_vec(first(cellvec))
+        rows1 = getindex!(rows_cache,cellids,1)
+        touch! = TouchEntriesMap()
+        touch_cache = return_cache(touch!,b,vec1,rows1)
+        caches = touch_cache, rows_cache
+        Gridap.FESpaces._symbolic_loop_vector!(b,caches,cellids,vec1,z,w)
+      end
+    end
+    b
+end
+
+
+@noinline function Gridap.FESpaces._symbolic_loop_vector!(
+  A, caches, cell_rows, vec1, z, w
+  )
+  cells = zip(z,w)
+  touch_cache, rows_cache = caches
+  touch! = TouchEntriesMap()
+  for cell in cells
+    rows = getindex!(rows_cache,cell_rows,cell[1])
+    evaluate!(touch_cache,touch!,A,vec1,rows)
+  end
+end
+
+#endregion
+
+##
+
+
+
+##
+
+#region ECM Solver
+
+function POD_ECM_Increment_Solver(
+    x0,step,nsteps,𝜑ᵇ,f,
+    model, Ω, dΩ,
+    cache,x_list,b_list,
+    bisect,
+    𝛷, z, w,
+    trace=true
+    )
+    prev_step = step-(1/2^bisect)
+    x0_copy = copy(x0)
+    n, k_l = size(𝛷)
+    b_POD = Vector{Float64}(undef,k_l)
+    K_T_POD_pos_mult = Matrix{Float64}(undef,n,k_l)
+    K_T_POD = Matrix{Float64}(undef,k_l,k_l)
+    res, jac = get_symbolic_res_and_jac(dΩ,f)
+    dirichletbc = get_DirichletBC(𝜑ᵇ*(step/nsteps))
+    fe_spaces = get_fe_spaces(model,dirichletbc)
+    norm_res = 1
+    count = 0
+    if trace
+        println("\n==============================================")
+    end
+    println("Material parameter = $f :: step = $step of $nsteps k+l = $k_l")
+    while norm_res>1e-12
+        if count>20 || norm_res>1e5
+            bisect += 1
+            if bisect > 5
+                throw("bisect = $bisect is too high")            
+            end
+            if trace
+                println("bisect = $bisect")
+            end
+            x0 = x0_copy
+            for i in 1:2^bisect
+                x0, cache, _, _ = POD_Increment_Solver(
+                    x0,prev_step+(i/2^bisect),nsteps,𝜑ᵇ,f,
+                    model, Ω, dΩ,
+                    cache,x_list,b_list,
+                    bisect,
+                    𝛷,
+                    trace
+                    )
+                if prev_step+(i/2^bisect)>step
+                    break                    
+                end
+            end
+            count = 0
+        end
+        ph = FEFunction(fe_spaces.U, 𝛷*x0)
+        b, K_T = get_numeric_res_and_jac(ph,fe_spaces,Ω,res,jac,z,w)
+        mul!(b_POD,𝛷',b)
+        mul!(K_T_POD_pos_mult,K_T,𝛷)
+        mul!(K_T_POD,𝛷',K_T_POD_pos_mult)
+        Δx = K_T_POD\(-b_POD)
+        copyto!(x0,x0+Δx)
+        norm_res = maximum(abs.(b_POD))
+        count += 1
+        a1 = log10(norm_res)
+        a2 = log10(norm(Δx))
+        if trace
+            print("\riter = $count norm_res = $(round(10.0^(a1-trunc(a1)),digits = 3))e$(Int64(trunc(a1))) norm_Δx = $(round(10.0^(a2-trunc(a2)),digits = 3))e$(Int64(trunc(a2)))")
+        end
+        if norm_res<1e-12
+            push!(x_list,copy(x0))
+            push!(b_list,copy(b_POD))
+        end
+    end
+    return x0, cache, x_list, b_list
+end
+
+function POD_Incremental_Solver(𝛷,f,trace=true)
+    model, Ω, dΩ = nothing, nothing, nothing
+    lock(gmsh_lock) do
+        model, Ω, dΩ =  get_trian_and_measure()
+    end
+    nsteps = 300
+    𝜑ᵇ = 5000.0
+    n, k_l = size(𝛷)
+    x0 = zeros(Float64,k_l)
+    x_list = []
+    b_list = []
+    cache = nothing
+    bisect = 0
+    for step in 1:nsteps
+        x0, cache, x_list, b_list = POD_Increment_Solver(
+            x0,step,nsteps,𝜑ᵇ,f,
+            model, Ω, dΩ,
+            cache,x_list,b_list,
+            bisect,
+            𝛷,
+            trace
+        )
+    end
+    return x_list, b_list
+end
+
+#endregion
+
+##
+
 
 ##
 
@@ -602,3 +950,46 @@ glo_res_element_contributions[1]
 
 
 ##
+
+
+##
+
+#region Assembly of the contributions matrix for ECM
+
+k,l = 17, 12
+f = 0.9
+
+folder = "scripts/MB Ex 3/POD_red_Solutions/V1_r_contributions/"
+
+sol = jldopen(folder*"MaterialParameter$f/RedParam_k_$(k)_l_$(l)_f_$f.jld2")
+
+sol["r_contri_list"][1][1]
+
+C = reduce(vcat,reduce.(hcat,sol["r_contri_list"]))
+
+function Assemble_contributions()
+    folder = "scripts/MB Ex 3/POD_red_Solutions/V1_r_contributions/"
+    k,l = 17, 12
+    f_list = [0.9,1.0,1.1]
+    C_list = []
+    for f in f_list
+        sol = jldopen(folder*"MaterialParameter$f/RedParam_k_$(k)_l_$(l)_f_$f.jld2")
+        C_ = reduce(vcat,reduce.(hcat,sol["r_contri_list"]))
+        push!(C_list,C_)
+    end
+    C = reduce(vcat,C_list)
+    b = vec(sum(C,dims=2))
+    return C
+end
+
+C, b = Assemble_contributions()
+
+u = RondomizedSVD(C,1e-8)
+
+z, w =ECM_Selection(u, 1e-6)
+
+#endregion
+
+
+##
+
